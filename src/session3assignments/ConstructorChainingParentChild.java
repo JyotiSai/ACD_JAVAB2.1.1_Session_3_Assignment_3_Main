e54595b62b5102c2795db9ff2ca2f6b7a7b16ea1
JyotiSai <kajal.jojo@gmail.com>
2017-12-11 02:11:53 +0530

    Update ConstructorChainingParentChild.java

--- src/session3assignments/ConstructorChainingParentChild.java
+++ src/session3assignments/ConstructorChainingParentChild.java
@@ -1,25 +1,26 @@
 package session3assignments; //Package declaration
 
 /**
- * This child class demonstrates Constructor Chaining between parent and child class using super keyword
+ * This child class demonstrates Constructor Chaining between parent and child
+ * class using super keyword
  *
  */
-public class ConstructorChainingParentChild extends ConstructorChainingSameClass
-{
-	public ConstructorChainingParentChild() //Default Constructor
+public class ConstructorChainingParentChild extends ConstructorChainingSameClass {
+	public ConstructorChainingParentChild() // Default Constructor
 	{
-		super(9); //Calling single parameterized constructor of parent class using super keyword
+		super(9); // Calling single parameterized constructor of parent class using super keyword
 		System.out.println("Child default constructor called.");
-	}//End of default constructor
-	
-	public ConstructorChainingParentChild(String name) //Single parameterized constructor of child class 
+	}// End of default constructor
+
+	public ConstructorChainingParentChild(String name) // Single parameterized constructor of child class
 	{
-		super(); //Calling default constructor of parent class using super keyword
-		System.out.println("Child parameterized constructor called: " +name); //Printing the name
-	} //End of parameterized constructor
-	public static void main(String [] args) //main method
+		super(); // Calling default constructor of parent class using super keyword
+		System.out.println("Child parameterized constructor called: " + name); // Printing the name
+	} // End of parameterized constructor
+
+	public static void main(String[] args) // main method
 	{
-		new ConstructorChainingParentChild(); //Call to default constructor of child class
-		new ConstructorChainingParentChild("Joy"); //Call to parameterized constructor of child class
-	} //End of main method
-} //End of class
+		new ConstructorChainingParentChild(); // Call to default constructor of child class
+		new ConstructorChainingParentChild("Joy"); // Call to parameterized constructor of child class
+	} // End of main method
+} // End of class
